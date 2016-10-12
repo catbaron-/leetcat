27975ccedf5f893a69a38888a0eeb8f72b6b1c5b
catbaron- <catbaron@live.cn>
2016-10-12 14:45:54 +0900

    Update: v.0.2: A O(n) algorithm using hash table.

--- 1. Two Sum/solution.py
+++ 1. Two Sum/solution.py
@@ -5,7 +5,6 @@
 # Given nums = [2, 7, 11, 15], target = 9,
 # Because nums[0] + nums[1] = 2 + 7 = 9,
 
-return [0, 1].
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -13,8 +12,9 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in xrange(len(nums)):
-            for j in xrange(i+1, len(nums)):
-                if nums[i]+nums[j] == target:
-                    return [i, j]
-        return []
+        dic = {}
+        for i, ni in enumerate(nums):
+            if ni in dic:
+                return[i, dic[ni]]
+            else:
+                dic[target - ni] = i
